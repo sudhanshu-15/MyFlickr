813353ec01e0f7edba55b671a70db14776efd1a5
Sudhanshu Siddh <ssiddh@iu.edu>
2018-12-06 18:23:48 -0500

    Skeleton code for AppComponent

---
+++ app/src/main/java/me/ssiddh/flickrapp/di/AppComponent.java
@@ -0,0 +1,28 @@
+package me.ssiddh.flickrapp.di;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjectionModule;
+import me.ssiddh.flickrapp.FlickrApplication;
+
+//TODO: Add appmodule and activity modules
+@Singleton
+@Component(modules = {
+        AndroidInjectionModule.class
+})
+public interface AppComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+
+    void inject(FlickrApplication application);
+}
