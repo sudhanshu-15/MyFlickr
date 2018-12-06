13ddbabbb6d8141e2d57998a61fbdf76076657ec
Sudhanshu Siddh <ssiddh@iu.edu>
2018-12-06 18:32:54 -0500

    Added retrofit provides in AppModule

---
+++ app/src/main/java/me/ssiddh/flickrapp/di/AppModule.java
@@ -0,0 +1,31 @@
+package me.ssiddh.flickrapp.di;
+
+import android.arch.lifecycle.ViewModelProvider;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import me.ssiddh.flickrapp.BuildConfig;
+import me.ssiddh.flickrapp.api.FlickrService;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module(subcomponents = ViewModelSubComponent.class)
+class AppModule {
+
+    @Provides
+    Executor provideExecutor() {
+        return Executors.newSingleThreadExecutor();
+    }
+
+    @Singleton
+    @Provides
+    FlickrService provideFlickrService() {
+        return new Retrofit.Builder().baseUrl(BuildConfig.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build().create(FlickrService.class);
+    }
+
+}
