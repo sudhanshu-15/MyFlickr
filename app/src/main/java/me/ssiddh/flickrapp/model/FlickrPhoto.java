2abe0eb426d805ec4a90651e5469dd31bd1aa596
Sudhanshu Siddh <ssiddh@iu.edu>
2018-12-06 17:53:57 -0500

    Created model for individual photo

---
+++ app/src/main/java/me/ssiddh/flickrapp/model/FlickrPhoto.java
@@ -0,0 +1,55 @@
+package me.ssiddh.flickrapp.model;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.annotations.SerializedName;
+
+public class FlickrPhoto {
+
+    @NonNull
+    private int id;
+    private String server;
+    private String title;
+    @SerializedName("url_m")
+    private String url;
+
+    public FlickrPhoto(@NonNull int id, String server, String title, String url) {
+        this.id = id;
+        this.server = server;
+        this.title = title;
+        this.url = url;
+    }
+
+    @NonNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
