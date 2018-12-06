1face10c39dd116cc4eb4830d48841636abec075
Sudhanshu Siddh <ssiddh@iu.edu>
2018-12-06 18:27:59 -0500

    Adding Activity injector to base application

--- app/src/main/java/me/ssiddh/flickrapp/FlickrApplication.java
+++ app/src/main/java/me/ssiddh/flickrapp/FlickrApplication.java
@@ -1,11 +1,25 @@
 package me.ssiddh.flickrapp;
 
+import android.app.Activity;
 import android.app.Application;
 
-public class FlickrApplication extends Application {
+import javax.inject.Inject;
+
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class FlickrApplication extends Application implements HasActivityInjector {
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
 
     @Override
     public void onCreate() {
         super.onCreate();
+    }
+
+    @Override
+    public DispatchingAndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
     }
 }
